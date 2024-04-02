73757ee7b6572d9e98e10e43a7e1f0ba2222d3d5
GenaPlem <shainkill1337@gmail.com>
2024-04-02 11:50:07 +0100

    Update timer functionality to match real time

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,45 +1,51 @@
 import {useState, useRef, useEffect} from "react";
+import {formatTime} from "../utils/formatTime";
 
 export const Timer = () => {
-    const [timerValue, setTimerValue] = useState(0);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const startTimeRef = useRef(null);
     const intervalRef = useRef(null);
 
     const handleStart = () => {
-        if (intervalRef.current === null) {
+        if (!intervalRef.current) {
+            const now = Date.now();
+            startTimeRef.current = now - elapsedTime; // Устанавливаем начальное время с учётом прошедшего
             intervalRef.current = setInterval(() => {
-                setTimerValue((prevState) => prevState + 1);
+                setElapsedTime(Date.now() - startTimeRef.current);
             }, 1000);
         }
     };
 
     const handleStop = () => {
-        if (intervalRef.current !== null) {
+        if (intervalRef.current) {
             clearInterval(intervalRef.current);
             intervalRef.current = null;
         }
     };
 
     const handleReset = () => {
-        if (intervalRef.current !== null) {
+        if (intervalRef.current) {
             clearInterval(intervalRef.current);
-            intervalRef.current = null;
-            setTimerValue(0);
-        } else {
-            setTimerValue(0);
         }
+
+        intervalRef.current = null;
+        startTimeRef.current = null;
+        setElapsedTime(0);
     }
 
     useEffect(() => {
         return () => {
-            if (intervalRef.current !== null) {
+            if (intervalRef.current) {
                 clearInterval(intervalRef.current);
             }
         }
     }, []);
 
+    const formattedTime = formatTime(elapsedTime);
+
     return (
         <>
-            <h1>{timerValue}</h1>
+            <h1>{formattedTime}</h1>
             <button onClick={handleStart}>Start</button>
             <button onClick={handleStop}>Stop</button>
             <button onClick={handleReset}>Reset</button>
